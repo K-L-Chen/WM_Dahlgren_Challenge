8d60a3f844ed0d9c6665e0cc979278ec70b165ec
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2023-02-25 22:41:28 -0500

    Implement ActionRule

--- Code/Planner/Example_Clients/MetaheuristicClient/ActionRule.py
+++ Code/Planner/Example_Clients/MetaheuristicClient/ActionRule.py
@@ -13,6 +13,7 @@
 """
 import numpy as np
 
+
 class ActionRule:
     # fields for updating ActionRule fitness
     predicted_value = 0.0
@@ -21,48 +22,51 @@ class ActionRule:
     accuracy = 0.0
     relative_accuracy = 0.0
 
-    def __init__(self):
-        #decision variables
-        self.distance_to_target = 0
-        self.target_speed = 0
-        self.target_heading = 0 #abs value of angle
-        self.target_height = 0
-        
-        #most important decision vars
-        self.nearby_ships = 0
-        self.nearby_weapons = 0
-        self.nearby_ship_health = 0
-        self.my_ship_health = 0
+    def __init__(self, attr=None, cond_bits=None):
+        """
+        Constructor for ActionRule
 
-        #make vectors for attr name, vec
-        #think about making them into dataframes
-        self.attr_name_vec = np.array(["distance_to_target", "target_speed", "target_heading", "target_height", "nearby_ships", "nearby_weapons", "nearby_ship_health", "my_ship_health"])
-        self.attr_vec = np.zeros(8)
+        @param attr: An optional parameter, if not None, are a numpy array of values used to initialize this ActionRule.
+        @param cond_bits: An optional parameter, if not None, is an integer encoding the conditionals detailed below.
+        """
 
-        # TODO consider AND vs OR style of policy, use greater than or less than
-        # store all of our AND/OR, GE/LE decisions as 2 bit values in a long(?)
-        # start at the right hand side, so:
-        # distance_to_target is at bits 1 and 0
-        # target_speed is at bits 3 and 2, etc...
-        # odd num bits are AND/OR -> 0/1
-        # even num bits are GE/LE -> 0/1
-        # to change values, place all bits we want to flip into a number
-        # then XOR that number with conditional_bits
-        # e.g. we want to flip bit 3, then we XOR conditional_bits with 0x4
-        self.conditional_bits = 0
+        # make vectors for attr name, vec
+        # think about making them into dataframes
+        self.attr_name_vec = np.array(
+            ["distance_to_target", "target_speed", "target_heading", "target_height", "nearby_ships", "nearby_weapons",
+             "nearby_ship_health", "my_ship_health"])
+
+        if attr is not None:
+            self.attr_vec = attr
+            self.conditional_bits = cond_bits
+        else:
+            self.attr_vec = np.zeros(8)
+
+            # store all of our AND/OR, GE/LE decisions as 2 bit values in a long(?)
+            # start on the right hand side, so:
+            # distance_to_target is at bits 1 and 0
+            # target_speed is at bits 3 and 2, etc...
+            # odd num bits are AND/OR -> 0/1
+            # even num bits are LE/GE -> 0/1
+            # to change values, place all bits we want to flip into a number
+            # then XOR that number with conditional_bits
+            # e.g. we want to flip bit 3, then we XOR conditional_bits with 0x4
+            self.conditional_bits = 0
 
         # TODO maybe PCA it if we have time
         # TODO optimize if/else chains with lists, vectors, or other ways
 
-        print("placeholder")
-
-    def update_predicted_values(self, reward):
+    def update_predicted_values(self, reward, step):
         """
         Given the reward from an action, update predicted_value, predicted_val_error, and accuracy. Return accuracy.
 
         @param reward: The reward quantity from an action
-        @return: accuracy value for an action, as a double
+        @param step: The step size for an update
+        @return: NOTHING FOR NOW — later: (accuracy value for an action, as a double)
         """
+
+        # right now, we are only updating predicted_value
+        self.predicted_value = self.predicted_value + step * (reward - self.predicted_value)
 
     def update_fitness(self, accuracy_sum):
         """
@@ -72,3 +76,39 @@ class ActionRule:
             same time step
         @return:
         """
+        # TODO: Consider extra fitness parameters
+
+    def update_conditional_attributes(self, update_value):
+        """
+        Updates the attributes of the conditionals encoded within this ActionRule
+        (e.g. AND vs OR and < or >)
+
+        @param update_value: The update to the conditional bits that we want to carry out — an integer interpreted as
+            a bitstring — use a 1 in the positions you want the bit to be flipped, and a 0 in the positions where you
+            would not want a bit to be flipped.
+
+            Example: If the conditional bits are 0101011, and the input value is 1001000, the updated conditional bits
+                will be 1100011.
+        @return: None
+        """
+        self.conditional_bits = self.conditional_bits ^ update_value
+
+    def get_conditional_attributes(self):
+        """
+        @return: The conditional attributes bitstring
+        """
+        return self.conditional_bits
+
+    def update_conditional_values(self, update_vec):
+        """
+        Updates the values encoded within the conditional.
+        @param update_vec: New values to use in the conditional, as a numpy vector.
+        @return: None
+        """
+        self.attr_vec = update_vec
+
+    def get_conditional_values(self):
+        """
+        @return: The values encoded within the conditional.
+        """
+        return self.attr_vec
